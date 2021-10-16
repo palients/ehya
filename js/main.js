3ebdd36dadc56846378331158eb5a775fd961dd5
palients <tacegml@gmail.com>
2021-10-16 19:12:39 +0300

    download from another computer

---
+++ js/main.js
@@ -0,0 +1,245 @@
+$(document).ready(function () {
+    var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function() {
+    console.log("Клик по кнопке меню");
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  });
+
+  var player;
+   $('.story-author__icon').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '100%',
+      width: '100%',
+      videoId: 'Ks8W7iCAYRI',
+        events: {
+          'onReady': videoPlay,
+        }
+      });
+    })
+
+    function videoPlay(event) {
+      event.target.playVideo();
+    }
+
+  var player;
+   $('.history__icon--bg-1').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player-1', {
+      height: '100%',
+      width: '100%',
+      videoId: 'Ks8W7iCAYRI',
+        events: {
+          'onReady': videoPlay,
+        }
+      });
+    })
+
+    function videoPlay(event) {
+      event.target.playVideo();
+    }
+
+  var player;
+   $('.history__icon--bg-2').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player-2', {
+      height: '100%',
+      width: '100%',
+      videoId: 'Ks8W7iCAYRI',
+        events: {
+          'onReady': videoPlay,
+        }
+      });
+    })
+
+    function videoPlay(event) {
+      event.target.playVideo();
+    }
+
+  var player;
+   $('.history__icon--bg-3').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player-3', {
+      height: '100%',
+      width: '100%',
+      videoId: 'Ks8W7iCAYRI',
+        events: {
+          'onReady': videoPlay,
+        }
+      });
+    })
+
+    function videoPlay(event) {
+      event.target.playVideo();
+    }
+
+    $('.single-item').slick({
+      infinite: false,
+      dots: false,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      responsive: [
+	    {
+	      breakpoint: 1300,
+	      settings: {
+	        slidesToShow: 4,
+	      }
+	    },
+	    {
+	      breakpoint: 1100,
+	      settings: {
+	        slidesToShow: 3,
+	      }
+	    },
+      {
+	      breakpoint: 900,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    },
+      {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    },
+    ]
+    });
+
+
+    $('.single-items').slick({
+      infinite: false,
+      dots: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+	    {
+	      breakpoint: 1400,
+	      settings: {
+	        slidesToShow: 3,
+	      }
+	    },
+	    {
+	      breakpoint: 1070,
+	      settings: {
+	        slidesToShow: 2,
+	      }
+	    },
+      {
+	      breakpoint: 992,
+	      settings: {
+          slidesToShow: 4,
+	        slidesToScroll: 2,
+          rows: 2,
+	      }
+	    },
+      {
+	      breakpoint: 768,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    },
+    ]
+    });
+
+     $('.single-ite').slick({
+      infinite: false,
+      dots: false,
+      rows: 2,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+    });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  var closeModalBackground = $(".modal__overlay");
+
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+  closeModalBackground.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+  $(document).on('keydown', function (e) {
+    if (e.which === 27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+  });
+
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Укажите ваше имя",
+          minlength: "Введите ваше настоящее имя",
+        },
+        email: {
+          required: "Укажите адрес электронной почты",
+          email: "Формат name@domain.com",
+        },
+        phone: {
+          required: "Укажите номер телефона",
+          minlength: "Некорректный номер"
+        },
+      },
+    });
+  });
+
+  $(".newsletter__subscribe").each(function () {
+    $(this).validate({
+      errorClass: "invalide",
+      messages: {
+        subscribe: {
+          required: "Укажите адрес электронной почты",
+          email: "Формат name@domain.com",
+        },
+      },
+    });
+  });
+
+  $(".form").ready(function () {
+    $('.modal__input--phone').mask('+7 (ZZZ) ZZZ-ZZ-ZZ', {
+      translation: {
+        'Z': {
+          pattern: /[0-9]/, optional: true
+        }
+      }
+    });
+  });
+  $(function() {
+    $(window).scroll(function() {
+  if($(this).scrollTop() != 0) {
+    $('#toTop').fadeIn();
+  } else {
+    $('#toTop').fadeOut();
+  }
+  });
+  $('#toTop').click(function() {
+  $('body,html').animate({scrollTop:0},800);
+  });
+  });
+
+  $(document).ready(function(){
+       $( ".card-info__love" ).click(function() {
+        if($(this).hasClass('iconHeartActive')) {
+            $( this ).removeClass( "iconHeartActive" );
+            $( this ).addClass( "iconHeartInactive" );
+        } else {
+            $( this ).removeClass( "iconHeartInactive" );
+            $( this ).addClass( "iconHeartActive" );
+        }
+
+     });
+ });
+});  
---
+++ send.php
@@ -0,0 +1,75 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+$subscribe = $_POST['subscribe'];
+$form = $_POST['form'];
+$thankyou = $_POST['thankyou'];
+
+
+// Формирование самого письма
+if($form == 'thankyou'){
+    // если есть что-то в $_POST['email']
+   $title = "Новое обращение EHYA";
+   $body = "
+   <h2>Новое обращение</h2>
+   <b>Имя:</b> $name<br>
+   <b>Телефон:</b> $phone<br>
+   <b>Почта:</b> $email
+   ";
+} elseif ($form == 'subscription') {
+    // если нет, отправлена форма с телефоном и пр.
+    $title = "Новая подписка EHYA";
+    $body ="
+    <h2>Новый подписчик</h2>
+    <b>Почта:</b> $subscribe<br><br>
+";
+}
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    // $mail->SMTPDebug = 2; 
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'mmotya.mmotya@mail.ru'; // Логин на почте
+    $mail->Password   = 'FWBDgjRwffSZR3Zjp6BQ'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('mmotya.mmotya@mail.ru', 'Moтя Виноградова'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('kevdina@yandex.ru');  
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+if ($form == 'thankyou') {
+    header('Location: appeal.html');
+}elseif ($form == 'subscription') {
+    header('Location: thankyou.html');
+}
