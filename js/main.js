dc2582541e92fed2c0f54af5e82e974009c7d563
palients <tacegml@gmail.com>
2021-10-17 16:24:53 +0300

    add: function / fix: adaptive 320px

--- js/main.js
+++ js/main.js
@@ -241,4 +241,11 @@ $(document).ready(function () {
 
      });
  });
+ $(document).ready(function(){
+   $("#menu").on("click", "a", function (event) {
+     event.preventDefault();
+     var id = $(this).attr("href"), top = $(id).offset().top;
+     $("body, html").animate({scrollTop: top}, 1500);
+     })
+ })
 });  
